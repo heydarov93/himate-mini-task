615f70e2cdb60c9d829ae818fc12c86b8c318421
heydarov93 <heyderov.yashar@mail.ru>
2024-09-12 21:39:14 +0400

    routes for settings and experience created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import Settings from "./pages/Settings";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import SettingsLayout from "./ui/SettingsLayout";
+import Experience from "./components/settings/settings-body/experience";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/settings",
+        element: <SettingsLayout />,
+        children: [
+          {
+            path: "profile",
+            element: <h1>profile</h1>,
+          },
+          {
+            path: "experience",
+            element: <Experience />,
+          },
+          {
+            path: "education",
+            element: <h1>education</h1>,
+          },
+          {
+            path: "preferences",
+            element: <h1>preferences</h1>,
+          },
+          {
+            path: "notifications",
+            element: <h1>notifications</h1>,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
-      <div>
-        <Settings />
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
