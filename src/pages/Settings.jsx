fad78b11fb1cadd27cb110cc25d7ff76fe3512e2
heydarov93 <heyderov.yashar@mail.ru>
2024-09-12 17:09:38 +0400

    file structures almost ready

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../components/sidebar/index";
+
+function Settings() {
+  return (
+    <div className="flex gap-16">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Settings;
